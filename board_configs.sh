495e5b19b5c9f79afd0ed84afd82ac5caea16506
Stephen Chen <stephen@radxa.com>
2024-06-19 14:22:48 +0800

    board_configs.sh: add UBOOT_SPI_DEFCONFIG for rock-5c
    Signed-off-by: Stephen Chen <stephen@radxa.com>

--- board_configs.sh
+++ board_configs.sh
@@ -342,6 +342,7 @@ case ${BOARD} in
 	"rock-5c")
 		DEFCONFIG=rockchip_linux_defconfig
 		UBOOT_DEFCONFIG=rock-5c-rk3588s_defconfig
+		UBOOT_SPI_DEFCONFIG=rock-5c-spi-rk3588s_defconfig
 		DTB=rk3588s-rock-5c.dtb
 		export ARCH=arm64
 		export CROSS_COMPILE=aarch64-none-linux-gnu-
